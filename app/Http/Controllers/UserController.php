0c637637b52665b671b99765c9cc6223f42fb241
Preeti <wwwwebexpert>
2019-03-28 15:55:19 +0530

    Laravel 5.7 with eloquent orm update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,30 +5,33 @@ use App\User;
 use App\Posting;
 use App\Expertise;
 use Illuminate\Http\Request;
-use DB; 
+ 
 class UserController extends Controller
 {
-    /*Function for execute query 1*/
+    /*Function for execute query 1
+    Task 1: Query the lists of postings that John Doe is matched and posting is done
+    */
     public function index(){
-
     
-    	$PostingDataQuery1 = Posting::select('id','name','status')->where('status','=','done')->with(['PostingWorkersMatched','PostingWorkersMatched.Users'])->get();
+    	$PostingDataQuery = Posting::select('id','name','status')->where('status','=','done')->with(['PostingWorkersMatched','PostingWorkersMatched.Users'])->get();
         
-    	echo "<pre>"; print_r($PostingDataQuery1);echo "<pre>";die;
-
+    	echo "<pre>"; print_r($PostingDataQuery);echo "<pre>";die;
     	
     }
-    /*Function for execute query 2*/
+    /*Function for execute query 2
+    Task 2 : Query the lists of postings that John Doe is fully available: 
+    */
     public function secondQuery(){
-
-        $PostingDataQuery2 = Posting::select('id','name','status')->where('status','=','available')->with(['PostingWorkers.Users'])->get();
+        $PostingDataQuery = Posting::select('id','name','status')->where('status','=','available')->with(['PostingWorkers.Users'])->get();
     	
-    	echo "<pre>"; print_r($PostingDataQuery2); echo "<pre>";die;
+    	echo "<pre>"; print_r($PostingDataQuery); echo "<pre>";die;
     }
 
-    /*Function for execute query 3*/
+    /*Function for execute query 3
+    Task 3: Create an Eloquent ORM where it provides the (id, name,posting_count) of all expertises
+    */
     public function thirdQuery(){
-        $ExpertiseDataQuery3  = Expertise::select('id','name')->with(['PostingCount'])->get();
-    	echo "<pre>";print_r($ExpertiseDataQuery3);die;
+        $ExpertiseDataQuery  = Expertise::select('id','name')->with(['PostingCount'])->get();
+    	echo "<pre>";print_r($ExpertiseDataQuery);die;
     }
 }
